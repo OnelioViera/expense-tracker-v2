41f35af4234282c0fc90bee0fca99f21b5506c6e
Onelio Viera <ojvwebdesign@gmail.com>
2024-07-03 18:40:06 -0600

    chore: Update database URL in drizzle.config.js and utils/db.ts

--- drizzle.config.js
+++ drizzle.config.js
@@ -8,7 +8,7 @@ export default {
   out: "./migrations",
   dialect: "postgresql",
   dbCredentials: {
-    url: 'postgresql://neondb_owner:UuwWN81nmaTF@ep-solitary-heart-a6dgb4po.us-west-2.aws.neon.tech/neondb?sslmode=require',
+    url: process.env.NEXT_PUBLIC_DATABASE_URL,
   },
 };
 
